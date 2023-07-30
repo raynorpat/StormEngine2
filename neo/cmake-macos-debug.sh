f51b4b3451e8a1f18d434ce577539bd1f1b2d0ef
Pat Raynor <raynorpat@raynorpat.com>
2023-07-29 23:05:17 -0400

    Add ability to build under macOS - note that building ARM binaries won't happen due to the use of SSE intrinsics everywhere currently

---
+++ neo/cmake-macos-debug.sh
@@ -0,0 +1,5 @@
+cd ..
+rm -rf build
+mkdir build
+cd build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_OSX_DEPLOYMENT_TARGET=13.0 -DOPENAL_LIBRARY=/opt/homebrew/opt/openal-soft/lib/libopenal.dylib -DOPENAL_INCLUDE_DIR=/opt/homebrew/opt/openal-soft/include ../neo
